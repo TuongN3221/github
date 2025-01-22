514192ca786621c55162b82692b1bde587189a2b
Tuong Nguyen <yolotube21@yahoo.com>
2025-01-21 19:42:11 -0500

    Task 1 - Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,15 @@
+let employeeName = "Fish Face";
+const employeeID = 1337;
+var isActive = true;
 
+// console logging employeeName
+console.log("Employee:", employeeName);
+console.log("Type:", typeof employeeName);
+
+// console logging  employeeID
+console.log("ID:", employeeID);
+console.log("Type:", employeeID);
+
+//console logging isActive
+console.log("Active:", isActive);
+console.log("Type:", isActive);
